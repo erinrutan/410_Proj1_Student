596c4fc8e5b8604e7b7ab8d6c443f49506bd8138
Erin Rutan <erin.rutan.17@cnu.edu>
2020-09-19 19:06:21 -0400

    backup

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -9,6 +9,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <iterator>
 #include "utilities.h"
 #include "stdlib.h"
 
@@ -22,8 +23,53 @@ int stringToInt(const char *myString) {
 	return atoi(myString);
 }
 
-int loadData(const char* filename, bool ignoreFirstRow) {
+std::vector<process_stats> data;
 
+int loadData(const char* filename, bool ignoreFirstRow) {
+	ifstream myfile;
+	myfile.open(filename);
+	if (!myfile.is_open()) {
+		return COULD_NOT_OPEN_FILE;
+	}
+
+	string line;
+	while (getline(myfile, line)) {
+		istringstream ss(line);
+		process_stats temp;
+		string tempVal;
+		if (ignoreFirstRow) {
+			string ignoreLine;
+			//delete the first row of data
+			getline(ss, ignoreLine);
+		}
+		getline(ss, tempVal, ',');
+		if (tempVal == "" || tempVal.find(" ")) {
+			//skip to next row, bad data
+			continue;
+		}
+		temp.process_number = stringToInt(tempVal.c_str());
+		getline(ss, tempVal, ',');
+		if (tempVal == "" || tempVal.find(" ")) {
+			//skip to next row, bad data
+			continue;
+		}
+		temp.start_time = stringToInt(tempVal.c_str());
+		getline(ss, tempVal, ',');
+		if (tempVal == "" || tempVal.find(" ")) {
+			//skip to next row, bad data
+			continue;
+		}
+		temp.cpu_time = stringToInt(tempVal.c_str());
+		getline(ss, tempVal, ',');
+		if (tempVal == "" || tempVal.find(" ")) {
+			//skip to next row, bad data
+			continue;
+		}
+		temp.io_time = stringToInt(tempVal.c_str());
+		data.push_back(temp);
+	}
+
+	myfile.close();
 	return SUCCESS;
 }
 
@@ -41,7 +87,8 @@ process_stats getNext() {
 
 //returns number of process_stats structs in the vector holding them
 int getNumbRows(){
-	return 0;
+	return data.size();
+	//return 0;
 }
 
 
